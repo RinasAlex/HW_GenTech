0c37304b613dbd59f273ca2e07883516a74634a2
RinasAlex <151784662+RinasAlex@users.noreply.github.com>
2024-03-07 21:26:46 +0100

    Add files via upload

---
+++ hw-3/hw.sql
@@ -0,0 +1,32 @@
+-- Вывести данные о товарах не из категорий 2 и 4 и не от поставщиков 2 и 3 с ценой от 20 до 90 EUR (проекция: название, ценаснаценкой_6%)
+
+SELECT 
+ProductName,
+Price * 1.06 AS Price_up
+FROM Products
+WHERE
+NOT CategoryID	IN (2, 4)
+AND
+NOT SupplierID IN (2, 3)
+AND
+Price BETWEEN 20 AND 90
+
+-- Вывести клиентов не из Germany, имена которых не заканчиваются на букву t
+
+SELECT * 
+FROM Customers
+WHERE
+NOT Country = 'Germany'
+AND
+CustomerName NOT LIKE '%t'
+
+-- Вывести два самых дешевых товара с ценой от 10 EUR и более (проекция: название, ценасоскидкой_20%)
+
+SELECT 
+ProductName,
+Price * .80 AS Price_down
+FROM Products
+WHERE
+Price >= 10
+ORDER BY Price
+LIMIT 2 
